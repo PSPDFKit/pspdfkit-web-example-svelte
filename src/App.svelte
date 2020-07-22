63c8ac681b4d8379571f2ba471390fe7f9a50128
PSPDFKit <support@pspdfkit.com>
2020-07-22 15:41:23 +0200

    PSPDFKit for Web Svelte Example

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import PSPDFKit from "./PSPDFKit.svelte";
+
+  const LICENSE_KEY = process.env.PSPDFKIT_LICENSE_KEY;
+  let doc = "example.pdf";
+  let errorMsg = "";
+
+  function openDocument(e) {
+    pdf = window.URL.createObjectURL(e.target.files[0]);
+  }
+</script>
+
+<style>
+  :global(body) {
+    height: 100vh;
+    margin: 0;
+  }
+</style>
+
+<form>
+  <input type="file" on:change={openDocument} accept="application/pdf" />
+</form>
+<span class="error">{errorMsg}</span>
+<PSPDFKit {doc} licenseKey={LICENSE_KEY} bind:errorMsg />
