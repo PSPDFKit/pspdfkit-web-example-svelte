2be1cb4b65059e1255acc0984658bd08a393c13a
PSPDFKit <support@pspdfkit.com>
2022-03-04 18:59:39 +0100

    PSPDFKit for Web Svelte Example

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import PSPDFKit from "./PSPDFKit.svelte";
+
+  let doc = "example.pdf";
+  let errorMsg = "";
+
+  function openDocument(e) {
+    doc = window.URL.createObjectURL(e.target.files[0]);
+  }
+</script>
+
+<style>
+  :global(body) {
+    height: 100vh;
+    margin: 0;
+  }
+</style>
+
+<form>
+  <input type="file" on:change={openDocument} accept="application/pdf" />
+</form>
+<span class="error">{errorMsg}</span>
+<PSPDFKit {doc} bind:errorMsg />
